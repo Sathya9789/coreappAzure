45aba8a568287776caac69fa42d56bfe07acddcf
Sathya <Sathya26infotech.gmail.com>
2025-02-12 17:23:12 +0530

    Add project files.

---
+++ coreappAzure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using coreappAzure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace coreappAzure.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ coreappAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coreappAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
